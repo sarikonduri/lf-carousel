25995720e6a16ba58f348e4592ace0bd8291d375
Swarnalatha Konduri <sarikonduri@gmail.com>
2019-01-15 20:54:41 -0800

    Implements basic carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,29 +1,37 @@
 const defaultConfig = {
-    height: "200px",
+    height: "200",
+    interval: 5000, //5seconds
 };
 
 export class Carousel {
 
     get width() {
-        return this.element.offsetWidth + "px";
+        return this.element.offsetWidth;
     }
 
     constructor(config) {
+        this.activeItem = 0;
+
         this.config = Object.assign({}, defaultConfig, config);
 
         this.element = document.getElementById(this.config.id);
+        this.totalItems = this.element.children.length;
 
         this.setupCarousel();
         console.log(this.element);
+
+        this.registerCarouselTimer();
     }
 
     setupCarousel() {
         this.element.classList.add("carousel");
-        this.element.style.height = this.config.height;
+        this.element.style.height = this.config.height + "px";
 
-        const carouselWindow = document.createElement("div");
+        this.carouselWindow = document.createElement("div");
 
-        carouselWindow.classList.add("carousel-window");
+        this.carouselWindow.classList.add("carousel-window");
+        const dots = document.createElement("div");
+        dots.classList.add("dots");
 
         Array.from(this.element.children).forEach(child => {
             const imageUrl = child.getAttribute("attr.image");
@@ -35,11 +43,45 @@ export class Carousel {
 
             child.appendChild(imageNode);
 
-            child.style.width = this.width;
+            child.style.width = this.width + "px";
 
-            carouselWindow.appendChild(child);
+            this.carouselWindow.appendChild(child);
+
+            const dot = document.createElement("div");
+            dot.classList.add("dot");
+
+            dots.appendChild(dot)
         });
 
-        this.element.append(carouselWindow);
+
+
+        this.element.append(this.carouselWindow);
+
+        //Add arrows
+        const leftArrow = document.createElement("div");
+        leftArrow.classList.add("left-arrow");
+
+        const rightArrow = document.createElement("div");
+        rightArrow.classList.add("right-arrow");
+
+
+
+        // this.element.append(leftArrow);
+        // this.element.append(rightArrow);
+    }
+
+    registerCarouselTimer() {
+        setInterval(this.changeCarousel.bind(this), this.config.interval);
+    }
+
+    changeCarousel() {
+        this.activeItem++;
+        this.activeItem %= this.totalItems;
+
+        this.setItem(this.activeItem);
+    }
+
+    setItem(index) {
+        this.carouselWindow.style.marginLeft = (- (this.width * this.activeItem)) + "px";
     }
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import { Carousel } from "../components/carousel/carousel";
 
 const config = {
     id: "lf-hero-carousel",
+    height: 600,
 };
 
 const carousel = new Carousel(config);
