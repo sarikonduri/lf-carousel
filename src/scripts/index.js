f7e7c3cf89bf65849c3395b194874d8e0401f62b
Swarnalatha Konduri <sarikonduri@gmail.com>
2019-01-15 21:47:58 -0800

    Final cosmetic changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,7 @@ import { Carousel } from "../components/carousel/carousel";
 
 const config = {
     id: "lf-hero-carousel",
-    height: 600,
+    height: window.innerWidth < 400 ? window.innerWidth : 400,
 };
 
 const carousel = new Carousel(config);
